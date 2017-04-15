8791f6c79ff575fd309dccc39bda55f5e979146d
Anas Elkinawy <anas.aim111@gmail.com>
2017-04-15 07:39:52 +0200

    Update secret message.py

--- secret message.py
+++ secret message.py
@@ -9,9 +9,11 @@ def rename_file () :
     #changing the directory to the file path
     os.chdir("/media/username/partition/prank")
 
-    #rename folders :-
+    #making for loop to loop all the files in the list_files :-
     for file_name in file_list :
         # str.maketrans() that takes the characters-to-delete (the third argument)
         map = str.maketrans("","","1234567890")
+        #renamig files using translate function taking one argument
         os.rename(file_name,file_name.translate(map))
+
 rename_file()
