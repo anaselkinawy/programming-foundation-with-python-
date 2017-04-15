a406b3be204ab43eeab3de2acc26c0e9d94a5e72
Anas Elkinawy <anas.aim111@gmail.com>
2017-04-15 03:39:20 +0200

    Update open browser and taking break.py

--- open browser and taking break.py
+++ open browser and taking break.py
@@ -2,13 +2,12 @@ import webbrowser
 import time
 total_breaks= 3
 break_count = 0
-#making a loop to repeat taking break 3 times 
+#making a loop to repeat taking break 3 times
 while (break_count < total_breaks) :
-    #time befor start 
-    time.sleep(3)
-    #opening the url we want 
+    #time befor each start to break
+    time.sleep(3*60*60)
+    #opening the url we want
     webbrowser.open("https://www.youtube.com/")
-    #increasing the break_count by one 
+    #increasing the break_count by one
     break_count +=1
-
 
